03c1268a70dd1fb476f6f8774ba8c89dbf6ee97a
Vitaliy Kulinich <vitaliy.kulinich88@gmail.com>
2022-07-18 02:05:29 +0300

    Done pagination first page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { TheMovieDBApi } from './js/fetchfilm';
 import { renderText } from './js/renderText';
-
 import { SearchFilms } from './js/querySearchFilm';
 import {pagination} from './js/pagination';
--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,10 +1,7 @@
 import Pagination from 'tui-pagination';
-import TheMovieDBApi from './fetchfilm';
-import cardTpl from '../templates/filmcard.hbs';
-import {renderFilmCard} from "./renderText";
-import {api} from './renderText';
-// const api = new TheMovieDBApi();
-// console.log(TheMovieDBApi);
+import { renderFilmCard } from "./renderText";
+import { api } from './renderText';
+
 
 const container = document.getElementById('tui-pagination-container');
 const pagination = new Pagination(container, {
@@ -12,7 +9,6 @@ const pagination = new Pagination(container, {
   visiblePages: 5,
   centerAlign: false,
   firstItemClassName: 1,
-  // lastItemClassName:
   template: {
     currentPage: '<a class="page-btn is-selected">{{page}}</a>',
     page: '<a class="page-btn">{{page}}</a>',
@@ -26,25 +22,45 @@ pagination.on('afterMove', event => {
   const currentPage = event.page;
   fetchPerPage(currentPage);
 });
-// console.log(api);
+
 async function fetchPerPage(page) {
-  // api.query = '';
-  // console.log( api.page);
   api.page = page;
-  // console.log( api.page);
   const response = await api.fetchTrendingFilms();
-  await renderFilmCard();
-  // clearContainer();
-  // appendGalleryMarkup(response.hits);
-  // api.innerHTML = cardTpl(response.results);
-  // console.log(cardTpl);
-  console.log(response.data.total_results);
+  await renderFilmCard(); 
   if (page === 1) pagination.reset(response.data.total_results);
 }
 document.onload = fetchPerPage(1);
 
 
-fanction find
+export function addSearch() {
+  const container = document.getElementById('tui-pagination-container');
+  const pagination = new Pagination(container, {
+    itemsPerPage: 20,
+    visiblePages: 5,
+    centerAlign: false,
+    firstItemClassName: 1,
+    template: {
+      currentPage: '<a class="page-btn is-selected">{{page}}</a>',
+      page: '<a class="page-btn">{{page}}</a>',
+      moveButton: `<button class="move-btn move-btn-{{type}}"></button>`,
+      disabledMoveButton:
+        '<button class="move-btn move-btn-{{type}} disabled" disabled></button>',
+      moreButton: '<a class="page-btn next-is-ellip last-child">...</a>',
+    },
+  });
+  pagination.on('afterMove', event => {
+    const currentPage = event.page;
+    fetchPerPage(currentPage);
+  });
+  
+  async function fetchPerPage(page) {
+    api.page = page;
+    const response = await api.fetchFilms();
+    await renderFilmCard();  
+    if (page === 1) pagination.reset(response.data.total_results);
+  }
+  document.onload = fetchPerPage(1);
+}
 
 
 
