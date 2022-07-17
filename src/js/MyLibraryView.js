9b0f304cfc4887ad74b33bb86c111c70a1f0f97f
platinadoc <culture_6@i.ua>
2022-07-17 19:24:01 +0300

    ft-14-15 fixed

--- src/js/MyLibraryView.js
+++ src/js/MyLibraryView.js
@@ -1,68 +1,91 @@
-import libraryPage from "../templates/library.hbs";
-import refs from "../services/refs";
-import onDeleteClick from "../helpers/onDeleteClick";
-import placeholder from "../components/PlaceholderLibraryView";
+import filmcard from '../templates/filmcard.hbs';
 
-const MyLibrary = {
-  getWatched() {
-    const data = JSON.parse(localStorage.getItem("watched")) || [];
-    refs.content.innerHTML = "";
-    if (data.length === 0) {
-      placeholder(refs.content, "watched");
-    }
-    this.myLibraryMarkUp(data, refs.content);
-    onDeleteClick();
-  },
+const moviesEl = document.querySelector('.js-home-page');
+const showWatchedBtnEl = document.querySelector('.watched-button');
+const showQueueBtnEl = document.querySelector('.queue-button');
 
-  getQueue() {
-    const data = JSON.parse(localStorage.getItem("queue")) || [];
-    refs.content.innerHTML = "";
-    if (data.length === 0) {
-      placeholder(refs.content, "queue");
-    }
-    this.myLibraryMarkUp(data, refs.content);
-    onDeleteClick();
-  },
+showWatchedBtnEl.addEventListener('click', onWatchedBtnClick);
+showQueueBtnEl.addEventListener('click', onQueueBtnClick);
 
-  myLibraryMarkUp(data, ref) {
-    const newData = data.map((item) => {
-      item.release_date = item.release_date.slice(0, 4);
-      return item;
-    });
-    const markUp = libraryPage(newData);
-    ref.insertAdjacentHTML("beforeend", markUp);
-  },
+// const moviesExample = [
+//   {
+//     adult: false,
+//     backdrop_path: "/9eAn20y26wtB3aet7w9lHjuSgZ3.jpg",
+//     genre_ids: [12, 28, 878],
+//     id: 507086,
+//     media_type: "movie",
+//     original_language: "en",
+//     original_title: "Jurassic World Dominion",
+//     overview: "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history’s most fearsome creatures.",
+//     popularity: 6252.796,
+//     poster_path: "/kAVRgw7GgK1CfYEJq8ME6EvRIgU.jpg",
+//     release_date: "2022-06-01",
+//     title: "Jurassic World Dominion",
+//     video: false,
+//     vote_average: 6.876,
+//     vote_count: 1418
+//   },
+//   {
+//     adult: false,
+//     backdrop_path: "/9eAn20y26wtB3aet7w9lHjuSgZ3.jpg",
+//     genre_ids: [12, 28, 878],
+//     id: 507086,
+//     media_type: "movie",
+//     original_language: "en",
+//     original_title: "Jurassic World Dominion",
+//     overview: "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history’s most fearsome creatures.",
+//     popularity: 6252.796,
+//     poster_path: "/kAVRgw7GgK1CfYEJq8ME6EvRIgU.jpg",
+//     release_date: "2022-06-01",
+//     title: "Jurassic World Dominion",
+//     video: false,
+//     vote_average: 6.876,
+//     vote_count: 1418
+//   }
+// ]
 
-  init() {
-    refs.form.classList.add("none");
-    refs.buttonBox.innerHTML = "";
+// localStorage.setItem('watched', JSON.stringify(moviesExample));
+// localStorage.setItem('queue', JSON.stringify([...moviesExample, ...moviesExample]));
 
-    const watchedButton = document.createElement("button");
-    watchedButton.type = "button";
-    watchedButton.classList.add("library__button");
-    watchedButton.classList.add("library__button--active");
-    watchedButton.textContent = "Watched";
-    watchedButton.addEventListener("click", () => {
-      queueButton.classList.remove("library__button--active");
-      watchedButton.classList.add("library__button--active");
-      this.getWatched();
-    });
+function onWatchedBtnClick() {
+  const movies = getWatchedMovies();
 
-    const queueButton = document.createElement("button");
-    queueButton.type = "button";
-    queueButton.classList.add("library__button");
-    queueButton.textContent = "Queue";
-    queueButton.addEventListener("click", () => {
-      watchedButton.classList.remove("library__button--active");
-      queueButton.classList.add("library__button--active");
-      this.getQueue();
-    });
+  markUpMovies(movies);
+}
 
-    const buttonsMarkup = [watchedButton, queueButton];
-    refs.buttonBox.append(...buttonsMarkup);
+function onQueueBtnClick() {
+  const movies = getQueueMovies();
 
-    this.getWatched();
-  },
+  markUpMovies(movies);
+}
+
+function getWatchedMovies() {
+  return JSON.parse(localStorage.getItem("watched"));
+}
+
+function getQueueMovies() {
+  return JSON.parse(localStorage.getItem("queue"));
+}
+
+function myLibraryMarkUp(data, ref) {
+  ref.innerHTML = '';
+
+  const markUp = filmcard(data);
+  ref.insertAdjacentHTML("beforeend", markUp);
+}
+
+function emptyListMarkUp(ref) {
+  ref.innerHTML = '<div>THIS LIST IS EMPTY</div>';
 };
 
-export default MyLibrary;
+function markUpMovies(movies) {
+  if (movies.length === 0) {
+    // render placeholder
+    console.log('render placeholder');
+    emptyListMarkUp(moviesEl);
+  } else {
+    myLibraryMarkUp(movies, moviesEl)
+  }
+}
+
+// export default MyLibrary;
