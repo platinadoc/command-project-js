90d0422372f328ca71a46ef053bd8bd8d38e9585
Vitaliy Kulinich <vitaliy.kulinich88@gmail.com>
2022-07-19 21:20:50 +0300

    fix pagination after merge

--- src/js/querySearchFilm.js
+++ src/js/querySearchFilm.js
@@ -11,6 +11,12 @@ const formEl = document.querySelector('.search__form');
 const inputEl = document.querySelector('.js-inputSearch');
 const mainListEl = document.querySelector('.js-home-page');
 
+let inputValue = ""
+inputEl.addEventListener("input", (event) => {
+inputValue = event.target.value;
+console.log(event.target.value);
+} )
+
 const container = document.getElementById('tui-pagination-container');
 const pagination = new Pagination(container, {
   itemsPerPage: 20,
@@ -38,10 +44,10 @@ async function renderSearchPerPage (page) {
 }
 export async function fetchPerPage(page) {
   api.page = page;
-  console.log("page", api.page);
+ 
   const response = await api.fetchFilms();
   renderFilmList(response.data.results);
-  console.log(response.data.total_pages);
+ 
   // onInputChange();  
   if (page === 1) pagination.reset(response.data.total_results);
   return response
@@ -49,32 +55,28 @@ export async function fetchPerPage(page) {
 
 
 const onInputChange = async evt => {
-  evt.preventDefault();
-  const inputValue = inputEl.value.trim();
+  evt.preventDefault(); 
   if (inputValue === '') {
     return;
   }
-  api.setQuery(inputValue);
-  console.log(inputValue);
-  console.log(api.query);
-  
+  api.setQuery(inputValue);  
   const responsePerPage = await fetchPerPage(1);
   renderFilmList(responsePerPage.data.results);
 
-  // const getQueryFilms = await api.fetchFilms();  
+ 
 }
 
 async function renderFilmList(films) {
 // const filmArray = await responsePerPage.data.results;
-console.log(films);
+
 const convertFilms = films.map(el => {
   if (el.poster_path) {
     // el.poster_path = `https://image.tmdb.org/t/p/w500${el.poster_path}`;
-    console.log(el.poster_path);
+  
     return {...el, poster_path: `https://image.tmdb.org/t/p/w500${el.poster_path}`}
   } else {
     // el.poster_path = placeholder;
-    console.log(el.poster_path);
+   
     return {...el, poster_path:placeholder}
   } 
   return el
@@ -84,7 +86,6 @@ if (films.length === 0) {
   return;
 }
 const textFilmRender = filmcard(convertFilms);
-console.log(convertFilms);
 mainListEl.innerHTML = textFilmRender;
 };
 
