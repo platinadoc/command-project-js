98ebf7326281f2f2a849e7d332ce23187189a9f8
Konstantin Popov <popov.k.g@gmail.com>
2022-07-20 00:31:26 +0300

    done refactoring code and modalClickOnText

--- src/js/modal-gallery.js
+++ src/js/modal-gallery.js
@@ -1,6 +1,6 @@
 import createModal from "../templates/modal-gallery.hbs";
-// import axios from "axios";
 import { TheMovieDBApi } from './fetchfilm';
+// import axios from "axios";
 
 const openModalEl = document.querySelector('.js-home-page');
 const modalBackdropEl = document.querySelector('[data-modal]');
@@ -9,82 +9,43 @@ const modalEl = document.querySelector('.modal__body');
 const outsideModalEl = document.querySelector(".backdrop");
 const body = document.querySelector("body");
 
-let movie_id = null;
-
-// class TheMovieDBApi {
-//     #BASE_URL = 'https://api.themoviedb.org/3';
-//     #API_KEY = 'df75de766bc216630e148042dff14934';
-//     constructor() {
-//         this.movie_id = null;
-//     }
-    
-//     async fetchFilmsById(movie_id) {
-//         const searchParams = new URLSearchParams({
-//             api_key: this.#API_KEY,
-//             language: 'en-US',
-//         });
-//         try {
-//             const response = await axios.get(
-//                 `${this.#BASE_URL}/movie/${movie_id}?${searchParams}`
-//             );
-
-//             // console.log(response.data);
-
-//             return response.data;
-//         } catch (error) {
-//             console.log(error);
-//         }
-//     }
-// }
-
-// const api = new TheMovieDBApi();
-
-// modalEl.innerHTML = createModal();
-
 openModalEl.addEventListener('click', onOpenModalClick);
 
 function onOpenModalClick(e) {
     if (e.target === e.currentTarget) return;
 
-    modalBackdropEl.classList.remove("is-hidden");
+    modalBackdropEl.classList.remove("visually__hidden");
     body.classList.add("no-scroll");
 
     closeModalBtnEl.addEventListener('click', onCloseModalClick);
     body.addEventListener('keydown', onEscapeBtnClick);
     outsideModalEl.addEventListener('click', onOutsideModalClick);
 
-    if (e.target.dataset.id) {
-        movie_id = Number(e.target.dataset.id);
+    const dataId = e.target.dataset;
 
-        console.log(e.target.dataset.id);
-
-    } else if (e.target.dataset.src) {
-        movie_id = Number(e.target.dataset.src);
-
-        console.log(e.target.dataset.src);
+    if (dataId.id) {
+        movie_id = Number(dataId.id);
+    } else if (dataId.src) {
+        movie_id = Number(dataId.src);
+    } else if (dataId.hero) {
+        movie_id = Number(dataId.hero);
+    } else if (dataId.genres) {
+        movie_id = Number(dataId.genres);
+    } else if (dataId.release) {
+        movie_id = Number(dataId.release);
+    } else if (dataId.vote) {
+        movie_id = Number(dataId.vote);
+    } else if (dataId.list) {
+        movie_id = Number(dataId.list);
     } else {
         return;
     }
- 
+
     renderModalCard(movie_id);
-
-    // const cardObj = api.fetchFilmsById(movie_id);
-    
-    // modalEl.innerHTML = createModal(cardObj.data.results);
-    
-    // cardObj.then(data => console.log(data));
-    // cardObj.then(data => console.log(data.poster_path));
-    // cardObj.then(data => console.log(data.title.));
-    // cardObj.then(data => console.log(data.vote_average));
-    // cardObj.then(data => console.log(data.vote_count));
-    // cardObj.then(data => console.log(data.popularity.toFixed(1)));
-    // cardObj.then(data => console.log(data.original_title.));
-    // cardObj.then(data => console.log(data.genres.map(el => el.name).join(', ')));
-
 }
 
 function onCloseModalClick() {
-    modalBackdropEl.classList.add("is-hidden");
+    modalBackdropEl.classList.add("visually__hidden");
     body.classList.remove("no-scroll");
 
     closeModalBtnEl.removeEventListener('click', onCloseModalClick);
